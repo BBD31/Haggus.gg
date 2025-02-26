d76e55f2f1bc72dd7e88710de806e2c0006da975
BBD31 <daydayda7@gmail.com>
2025-02-26 20:46:47 +0200

    Loger.lua

--- Hook/Loger.lua
+++ Hook/Loger.lua
@@ -1,5 +1,16 @@
-local Hook = "https://raw.githubusercontent.com/BBD31/Haggus.gg/refs/heads/main/Hook/Script.json"
-function Hook:Log(Shared)
-		Hook:(Shared)
+local Script = "https://raw.githubusercontent.com/BBD31/Haggus.gg/refs/heads/main/Hook/Script.json"
+local Wander = false
+local HWID = {}
+if Script.HWID == true then
+    Wander = true
 end
-Hook:Log(true)
+function HWID:Check(Value)
+    HWID = Value or Wander
+       require()
+	return(Value)
+end
+local wait = task.wait
+while Wander do 
+    wait(0.5) or task.wait(0.5)
+    HWID:Check(HWID)
+end
